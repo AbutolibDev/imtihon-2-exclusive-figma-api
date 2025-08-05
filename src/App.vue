e6f45034b0f49779aa1d4c3eca12c100ada0a6fa
AbutolibDev <begalievabutolib08@gmail.com>
2025-08-05 15:29:29 +0300

    exclusive

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-900">
+    <h1 class="text-4xl font-bold text-white">
+      Tailwind 3.4.17 Vue 3 da ishlayapti! 🎉
+    </h1>
+  </div>
+</template>
+
